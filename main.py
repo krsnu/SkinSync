b56ac1bc35524bb70680a1543365665a38728989
krsnu <tandle.krishanu@gmail.com>
2026-07-12 21:47:15 -0500

    Imported the dataset

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello word!')
+import numpy as np
+import torch
+from torch import data
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+import os
+
+data = []
+labels = []
+
+base_path = ''
